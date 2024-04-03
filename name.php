060777938f3ba57d82ff84386151d707c0a3fd97
Michael Morales <164463505+aedificationis@users.noreply.github.com>
2024-04-03 15:50:55 -0600

    Add files via upload

---
+++ name.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My Name and Date/Time</title>
+</head>
+<body>
+
+  <?php
+  // Get your name (replace "Your Name" with your actual name)
+  $name = "Michael";
+
+  // Get the current date and time with desired format
+  $date = date("F j, Y - g:i a"); // Wednesday, 03 April 2024 - 01:43 PM
+
+  // Display the name and date/time
+  echo "Hello, $name! Today's date and time is: $date";
+  ?>
+
+</body>
+</html>
